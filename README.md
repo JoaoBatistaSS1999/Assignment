c52d843c197cb4c61d46620d041306d00ebceec1
João Batista <jbsouzasantos1999@gmail.com>
2022-08-05 17:04:00 -0300

    .env api-keys, new readme

---
+++ README.md
@@ -0,0 +1,21 @@
+<div>
+  <h2>📝Features to add?</h2><br>
+  <ul>
+    <li>✨Button to change network</li><br>
+    <li>
+      ✨ Option for the user to display NFT of another account if he doesn't
+      have any
+    </li><br>
+    <li>
+      ✨ Create a whole nother website where the user can mine NFT and get fake
+      tokens with testnet ETH
+    </li><br>
+  </ul>
+
+  <hr>
+
+  <h3>🪲 Bugs to fix:</h3><br>
+  <ul>
+    <li>😨NFT display page stop showing NFTs after refreshing (Brave)</li><br>
+  </ul>
+</div>
--- src/index.tsx
+++ src/index.tsx
@@ -3,23 +3,15 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import { MoralisProvider } from "react-moralis";
 
-// require("dotenv").config();
-
-// const serverUrl = process.env["SERVER_URL"];
-// const serverUrl = process.env.SERVER_URL;
-// const appId = process.env.APP_ID;
-// const appId = process.env["APP_ID"];
+const serverUrl = process.env.REACT_APP_SERVER_URL!;
+const appId = process.env.REACT_APP_APP_ID!;
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    {/* <MoralisProvider serverUrl={serverUrl} appId={appId}> */}
-
-    <MoralisProvider
-      serverUrl='https://epwcnvcahohc.usemoralis.com:2053/server'
-      appId='yrxpjr90sSclnoVM49dOqfYYu8CmXbWA7iaKovqL'>
+    <MoralisProvider serverUrl={serverUrl} appId={appId}>
       <App />
     </MoralisProvider>
   </React.StrictMode>
